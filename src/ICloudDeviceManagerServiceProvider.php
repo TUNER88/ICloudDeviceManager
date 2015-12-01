340fe548afd3f52b9cb0c58f7f8015715bd8d46f
TUNER88 <anton.pauli@gmail.com>
2015-12-02 00:36:37 +0100

    init package

---
+++ src/ICloudDeviceManagerServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace TUNER88\ICloudDeviceManager;
+
+use Illuminate\Support\ServiceProvider;
+
+class ICloudDeviceManagerServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
